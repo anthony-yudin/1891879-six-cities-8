4dbce6aad923f39c697556d79faf495cc008d606
Anton <yudin-1337@yandex.ru>
2021-10-16 22:59:25 +0500

    Создание первых компонентов приложения

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,12 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import React from 'react';
+import Main from '../main/main';
+
+type typeAppProps = {
+  countsRent: number;
+}
+
+function App({countsRent}: typeAppProps): JSX.Element {
+  return <Main countsRent = {countsRent}/>;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  countsRent: 51,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App countsRent = {Setting.countsRent} />
   </React.StrictMode>,
   document.getElementById('root'));
