250f7c4313718db70c2ec19be474271dc68f0be5
Anton <yudin-1337@yandex.ru>
2021-11-07 22:13:51 +0500

    Добавил карту на основе библиотеки leaflet

--- project/src/constants/constants.tsx
+++ project/src/constants/constants.tsx
@@ -4,3 +4,5 @@ export enum AppRoutes {
   Favorites = '/favorites',
   Room = '/offer/:id',
 }
+
+export const URL_MARKER_DEFAULT = 'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
